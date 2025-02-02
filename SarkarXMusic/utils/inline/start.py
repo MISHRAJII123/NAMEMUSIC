97eb03f617f70ad278076279206d0f831aec505e
MISHRAJII123 <ccaddnew@gmail.com>
2025-02-02 23:00:41 +0530

    Update start.py

--- SarkarXMusic/utils/inline/start.py
+++ SarkarXMusic/utils/inline/start.py
@@ -22,11 +22,11 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_5"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
